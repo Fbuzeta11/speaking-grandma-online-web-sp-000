7183a4287e4818ef56d7404bf0d42907d079946e
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2019-12-26 15:39:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return  "I LOVE YOU TOO PUMPKIN!"
     elsif == "#{phrase}".uppercase
+    return "NO, NOT SINCE 1938!"
+  else  
   end
   end # Write a speak_to_grandma method.
 
