ab9020efd20d28ee0a7e91e02e9affa5eb51f1c8
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2019-12-26 15:22:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma
   if puts "I LOVE YOU GRANDMA!"
-    
+    puts "I LOVE YOU TOO PUMPKIN!"
   end # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
