3a58eebd69cca8ee9d5930fd06b3353cec507551
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2019-12-26 15:43:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return  "I LOVE YOU TOO PUMPKIN!"
-    elsif  "#{phrase}".uppercase
-    return "NO, NOT SINCE 1938!"
+    elsif  phrase == "Hi Nana, how are you?"
+    return "
   else  
     return "HUH?! SPEAK UP, SONNY!"
   end
